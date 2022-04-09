fb0625f0904014d0bb400d024e23d48371b24df5
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-04-09 09:00:31 +0100

    Hash Password

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,30 +1,43 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
+from .. import models, schemas, utils
 from ..database import get_db
 from .. import models, schemas
 
 router = APIRouter(prefix="/user", tags=["Users"])
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.CreateUserRequest, db: Session = Depends(get_db)):
-    """Create a new user
+    """
+    Create a new user
     
     user: dict
-        user bs
+        User Details
     db: Session
-        Database object
+        Database Object
 
     Watch out: if username exists, if email exists
     """
-    existing_user = db.query(models.User).filter(models.User.username == user.username).first()
+    # if username exits, return error
+    existing_user = (
+        db.query(models.User).filter(models.User.username == user.username).first()
+    )
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="username already exists")
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="username already exists"
+        )
 
-    existing_email = db.query(models.User).filter(models.User.email == user.email).first()
+    # if email exits, return error
+    existing_email = (
+        db.query(models.User).filter(models.User.email == user.email).first()
+    )
     if existing_email:
-        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="email already exists")
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="email already exists"
+        )
     
+    user.password = utils.hash_password(user.password)
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
